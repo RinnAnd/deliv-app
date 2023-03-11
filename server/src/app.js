b7478abff70d9165817407fa049d5f87197b47be
RinnAnd <107440163+RinnAnd@users.noreply.github.com>
2023-03-10 20:48:02 -0500

    shop slice working

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import shopRouter from './routes/shop.routes.js';
 import dishRouter from './routes/dish.routes.js';
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(shopRouter);
 app.use(dishRouter);
