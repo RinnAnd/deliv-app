34f2cdd76adb1f81244d47301b27e5125231e4a7
RinnAnd <107440163+RinnAnd@users.noreply.github.com>
2023-03-15 00:55:33 -0500

    fixed types

--- client/src/redux/types.ts
+++ client/src/redux/types.ts
@@ -14,12 +14,5 @@ export interface Shop {    // declare interface for the shops coming in
     name: string;
     cuisine: string;
     logo?: string;
-    Dishes?: {
-      dish_id: number;
-      title: string;
-      description: string;
-      price: number;
-      ingredients?: string[];
-      image?: string;
-    }[];
+    Dishes?: Dish[];
 }
